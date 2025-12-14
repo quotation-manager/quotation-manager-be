ce8afede0ffac419a628752b43d6d7d6ac4b0adc
rushilvora01 <rushil.vora01@gmail.com>
2025-12-14 23:30:19 +0530

    changed letter head

--- src/services/pdfService.js
+++ src/services/pdfService.js
@@ -16,7 +16,7 @@ const https = require('https');
  */
 const loadLetterheadImage = async () => {
   try {
-    const letterheadPath = path.join(__dirname, '../assets/Tulip_Letter_Head.jpg');
+    const letterheadPath = path.join(__dirname, '../assets/sunny_letter_head.jpeg');
     const imageBuffer = fs.readFileSync(letterheadPath);
     const base64 = imageBuffer.toString('base64');
     return `data:image/jpeg;base64,${base64}`;
